a9843a530fd220097e93fa7299d30b0eb0116d25
dmitriyzhariy <dimazar2004@gmail.com>
2024-04-01 20:13:56 +0300

    маршрут выводится на сайте

--- tests_with_maps/website/views.py
+++ tests_with_maps/website/views.py
@@ -1,39 +1,45 @@
 from flask import Blueprint, render_template
-from flask_login import current_user
 import folium
 import osmnx as ox
 import networkx as nx
-from .map_initialize import G_walk
 from IPython.display import IFrame
 
 views = Blueprint('views', __name__)
 
+def OsmidToCoords(graph, ids):
+    coords = []
+    for i in range(1, len(ids)):
+        coords.append([graph.nodes[ids[i]]['y'], graph.nodes[ids[i]]['x']])
+    return coords
+
 @views.route('/', methods=['GET', 'POST'])
 def traffic_controller():
-
-    # ox.config(log_console=True, use_cache=True)
-
-    # G_walk = ox.graph_from_place('Manhattan Island, New York City, New York, USA',
-    #                         network_type='walk')
     
-    # orig_node = ox.nearest_nodes(G_walk, Y=40.748441, X=-73.985664)
+    # test_dir = "mapObj.html"
+    # check_dir = "osmnx.html"
+    mapObj = folium.Map(location=[64.53821631881615, 40.513887405395515], zoom_start=15, width=1850, height=900)
+    
+    ox.config(log_console=True, use_cache=True)
 
-    # dest_node = ox.nearest_nodes(G_walk, Y=40.748441, X=-73.3)
+    G_walk = ox.graph_from_point((64.53821631881615, 40.513887405395515),
+                            network_type='walk')
+    
+    orig_node = ox.nearest_nodes(G_walk, Y=64.54307276785013, X=40.51783561706544)
+    dest_node = ox.nearest_nodes(G_walk, Y=64.53672646553242, X=40.531611442565925)
 
-    # route = nx.shortest_path(G_walk,
-    #                         orig_node,
-    #                         dest_node,
-    #                         weight='length')
+    route = nx.shortest_path(G_walk,
+                            orig_node,
+                            dest_node,
+                            weight='length')
+    osmnx_map = ox.plot_route_folium(G_walk, route)
 
-    # mapObj = ox.plot_route_folium(G_walk, route)
-    # print()
-    # print(type(mapObj), type(folium.Map(location=[64.5383, 40.5140],
-    #                     zoom_start=17, width=1850, height=910)))
-    # print()
+    folium.PolyLine(
+        locations=OsmidToCoords(G_walk, route)
+    ).add_to(mapObj)
 
+    # mapObj.save(test_dir)
+    # osmnx_map.save(check_dir)
 
-    mapObj = folium.Map(location=[64.5383, 40.5140],
-                        zoom_start=17, width=1850, height=910)
 
     # render the map object
     mapObj.get_root().render()
@@ -47,7 +53,5 @@ def traffic_controller():
     # derive the JavaScript to be rendered in the HTML body
     script = mapObj.get_root().script.render()
 
-    print(header, body_html, script)
-
-    return render_template('home.html', user=current_user, 
-                        header=header, body_html=body_html, script=script)
+    return render_template('home.html', header=header, 
+                            body_html=body_html, script=script)
